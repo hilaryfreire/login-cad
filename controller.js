4250c827a2f1b62f8551506c463bd339ab0702e3
HILARY CAROLINE FREIRE DE ABEU <hilary.caroline@alunoescolassociais.org.br>
2024-08-29 12:16:49 -0300

    fiz o readme do cpf

--- controller.js
+++ controller.js
@@ -3,7 +3,6 @@ function acessar() {
     // Obtém os valores dos campos de email e senha
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
- 
     // Verifica se os campos estão preenchidos
     if (!loginEmail || !loginSenha) {
         // Se algum campo estiver vazio, exibe um alerta
@@ -13,20 +12,22 @@ function acessar() {
         window.location.href = "cadastro.html";
     }
 }
- 
 // Array que armazena a lista de nomes de usuários
 var dadosLista = [];
 var EmailLista = [];
- var CPFlista = [];
+var CPFlista = [];
 // Função para salvar o nome do usuário na lista
 function salvarUser() {
     // Obtém o valor do campo de nome de usuário
     let nomeUser = document.getElementById("nomeUser").value;
     let emailUser = document.getElementById("emailUser").value;
     let CPFUser = document.getElementById("CPFUser").value;
- 
+
     // Verifica se o campo de nome não está vazio
-    if (nomeUser && emailUser) {
+    if (document.getElementById('emailUser').value == "" || document.getElementById('emailUser').value.indexOf('@') == -1 || document.getElementById('emailUser').value.indexOf('.') == -1) {
+        // Se alguma das condições acima for verdadeira, exibe um alerta
+        alert("Por favor, informe um e-mail válido");
+    } else if (nomeUser && emailUser && CPFUser) {
         // Adiciona o nome ao array de dadosLista
         dadosLista.push(nomeUser);
         EmailLista.push(emailUser);
@@ -39,92 +40,39 @@ function salvarUser() {
         document.getElementById("CPFUser").value = "";
     } else {
         // Se o campo estiver vazio, exibe um alerta
-        alert("Favor informa o nome cadastrado");
+        alert("Favor informa todos os campos para cadastrado");
     }
 }
- 
+
 // Função para criar e atualizar a lista de usuários na tela
 function criarLista() {
     // Cria o cabeçalho da tabela
-    let tabela = "<tr><th> Nome Usuário</th><th>E-mail</th><th>CPF</tr><th>Ações</th>";
- 
+    let tabela = "<tr><th> Nome Usuário</th><th>E-mail</th><th>CPF</th><th>Ações</th>";
+
     // Itera sobre todos os nomes na lista de dados
-    for (let i = 0; i <= (dadosLista.length - 1); i++)
-    for (let i = 0; i <= (EmailLista.length - 1); i++)
-    for (let i = 0; i <= (CPFlista.length - 1); i++)
-     {
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
         // Adiciona uma nova linha para cada nome com um botão de editar
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td>"  + EmailLista[i] +  "</td><td>"+ CPFlista[i]+ "</td><td><button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>Editar</button><button type='button' onclick='excluir(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + EmailLista[i] + "</td><td>" + CPFlista[i] + "</td><td><button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>Editar</button><button type='button' onclick='excluir(this.parentNode.parentNode.rowIndex)' class='btn-rosa'>excluir</button></td></tr>";
     }
- 
+
     // Atualiza o conteúdo da tabela com a nova lista
     document.getElementById("tabela").innerHTML = tabela;
 }
- 
+
 // Função para editar um nome na lista
 function editar(i) {
     // Preenche o campo de nome com o nome selecionado para edição
-    document.getElementById('nomeUser').value = dadosLista[i - 1];
-    document.getElementById('emailUser').value = EmailLista[i - 1];
-    document.getElementById('CPFUser').value = CPFlista[i - 1];
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    document.getElementById('emailUser').value = EmailLista[(i - 1)];
+    document.getElementById('CPFUser').value = CPFlista[(i - 1)];
     // seleciona para a edição
-    dadosLista.splice(i - 1, 1);
-    EmailLista.splice(i - 1, 1);
-    CPFlista.splice(i - 1, 1);
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+    EmailLista.splice(EmailLista[(i - 1)], 1);
+    CPFlista.splice(CPFlista[(i - 1)], 1);
     // Atualiza a lista exibida na tela após a remoção
 }
- 
- 
 // Função para excluir um nome na lista
-    function excluir(i) {
-        dadosLista.splice((i -1), 1);
-        dadosLista.splice((i -1), 1);
-        dadosLista.splice((i -1), 1);
-        document.getElementById("tabela").deleteRow(i);
-    }
-    // Função para validar o email
-function checarEmail() {
-    // Verifica se o campo de email está vazio ou não contém '@' ou '.'
-    if (document.forms[0].email.value == "" ||
-        document.forms[0].email.value.indexOf('@') == -1 ||
-        document.forms[0].email.value.indexOf('.') == -1) {
-       
-        // Se alguma das condições acima for verdadeira, exibe um alerta
-        alert("Por favor, informe um e-mail válido");
-       
-        // Retorna false para evitar o envio do formulário
-        return false;
-    } else {
-        // Se o email é válido, exibe um alerta de sucesso
-        alert("Email informado com sucesso");
-       
-        // Atualiza o conteúdo de um elemento com o valor do email inserido
-        document.getElementById('email').innerHTML = document.forms[0].email.value;
-    }
-    function validarCPF(cpf){
-        cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
-     
-        // Estrutura de decisão para verificar quantidade de dígitos e se todos os digitos são iguais
-        if(cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)){
-            return false;
-        }
-       
-        let soma = 0;
-        let resto;
-     
-        // Validando 10º digito do CPF - o primeiro digito verificador
-        for(let i=1;i <= 9;i++){
-            soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
-        }
-     
-        resto = (soma * 10) % 11;
-     
-        if((resto === 10) || (resto === 11)){
-            resto = 0;
-        }
-        if(resto !== parseInt(cpf.substring(9, 10))){
-            return false;
-        }
-    }
+function excluir(i) {
+    dadosLista.splice((i - 1), 1);
+    document.getElementById("tabela").deleteRow(i);
 }
- 
